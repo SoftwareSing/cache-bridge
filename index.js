c6f705b16cd90cc68f11190a7aeba5c8eca6d7a4
SoftwareSing <softwaresing@gmail.com>
2021-07-28 22:58:06 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+const Bridge = require('./lib/Bridge')
+const Cache = require('./lib/Cache')
+const CacheClient = require('./lib/CacheClient')
+const CacheLocker = require('./lib/CacheLocker')
+const Locker = require('./lib/Locker')
+const Store = require('./lib/Store')
+
+module.exports = {
+  Bridge,
+  Cache,
+  CacheClient,
+  CacheLocker,
+  Locker,
+  Store
+}

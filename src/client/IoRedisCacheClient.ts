935ca74faf4789ba188f9bee2fed42a94d834b60
SoftwareSing <softwaresing@gmail.com>
2023-05-06 16:02:49 +0800

    feat(IoRedisCacheClient): implement IoRedisCacheClient

--- src/client/IoRedisCacheClient.ts
+++ src/client/IoRedisCacheClient.ts
@@ -1,12 +1,12 @@
 import { type CacheClient } from '../CacheClient'
 
-export class RedisCacheClient implements CacheClient {
-  protected readonly redis: Redis
+export class IoRedisCacheClient implements CacheClient {
+  protected readonly redis: ioRedis
   protected useUnlink: boolean
 
-  constructor ({ client, useUnlink = false }: { client: Redis, useUnlink?: boolean }) {
-    if (!isRedis(client)) {
-      throw new Error('RedisCacheClient needs to be used with node-redis, but the provided client does not meet the requirements.\nIf you are using ioredis, please switch to IoRedisCacheClient.')
+  constructor ({ client, useUnlink = false }: { client: ioRedis, useUnlink?: boolean }) {
+    if (!isIoRedis(client)) {
+      throw new Error('IoRedisCacheClient needs to be used with ioredis, but the provided client does not meet the requirements.\nIf you are using node-redis, please switch to RedisCacheClient.')
     }
 
     this.redis = client
@@ -24,7 +24,7 @@ export class RedisCacheClient implements CacheClient {
   async getMany (keyList: string[]): Promise<Array<[string, string | undefined]>> {
     if (keyList.length < 1) return []
 
-    const valueList = await this.redis.mGet(keyList)
+    const valueList = await this.redis.mget(keyList)
 
     const result = new Array(keyList.length)
     for (let i = 0; i < result.length; i += 1) {
@@ -54,7 +54,7 @@ export class RedisCacheClient implements CacheClient {
   }
 
   async set (key: string, text: string, ttl: number): Promise<void> {
-    await this.redis.set(key, text, { PX: ttl })
+    await this.redis.set(key, text, 'PX', ttl)
   }
 
   async setMany (keyTextMap: Map<string, string>, ttl: number): Promise<void> {
@@ -63,31 +63,53 @@ export class RedisCacheClient implements CacheClient {
     // mSet can not use with ttl, so use multi
     const multi = this.redis.multi()
     for (const [key, text] of keyTextMap.entries()) {
-      multi.set(key, text, { PX: ttl })
+      multi.set(key, text, 'PX', ttl)
     }
     await multi.exec()
   }
 
   async setNotExist (key: string, text: string, ttl: number): Promise<boolean> {
-    const result = await this.redis.set(key, text, { PX: ttl, NX: true })
+    const result = await this.redis.set(key, text, 'PX', ttl, 'NX')
     return result !== null
   }
 }
 
-interface Multi {
-  set: (k: string, t: string, o: { PX: number, NX?: true }) => any
+type ioDel1 = (...args: [...keys: string[]]) => Promise<any>
+type ioDel2 = (...args: [keys: string[]]) => Promise<any>
+
+type ioSet1 = (
+  key: string,
+  value: string,
+  millisecondsToken: 'PX',
+  milliseconds: number
+) => Promise<string | null>
+type ioSet2 = (
+  key: string,
+  value: string,
+  millisecondsToken: 'PX',
+  milliseconds: number,
+  nx: 'NX'
+) => Promise<string | null>
+
+interface ioMulti {
+  set: (
+    key: string,
+    value: string,
+    millisecondsToken: 'PX',
+    milliseconds: number
+  ) => any
   exec: () => Promise<any>
 }
 
-interface Redis {
+interface ioRedis {
   get: (k: string) => Promise<string | null>
-  mGet: (kList: string[]) => Promise<Array<string | null>>
-  unlink: (k: string | string[]) => Promise<any>
-  del: (k: string | string[]) => Promise<any>
-  set: (k: string, t: string, o: { PX: number, NX?: true }) => Promise<string | null>
-  multi: () => Multi
+  mget: (kList: string[]) => Promise<Array<string | null>>
+  unlink: ioDel1 & ioDel2
+  del: ioDel1 & ioDel2
+  set: ioSet1 & ioSet2
+  multi: () => ioMulti
 }
 
-function isRedis (r: any): r is Redis {
-  return typeof r.mGet === 'function'
+function isIoRedis (r: any): r is ioRedis {
+  return typeof r.mget === 'function'
 }
